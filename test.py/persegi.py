eecd72b282cb3d672b17639da5214d47c0f0f964
siti.faujiyyah_ti23@nusaputra.ac.id <siti.faujiyyah_ti23@nusaputra.ac.id>
2024-02-19 14:19:07 +0700

    tes commit

---
+++ test.py/persegi.py
@@ -0,0 +1,10 @@
+panjang = float(input ("masukan angka panjang: "))
+lebar = float (input ("masukan angka lebar : "))
+luas = 0.0
+keliling = 0.0
+
+rumusLuas = panjang * lebar
+rumusKeliling = 2 * panjang + lebar
+
+print("luasnya adalah : %.0f"%rumusLuas )
+print("kelilingnya adalah : %.0f"%rumusKeliling )
---
+++ test.py/test.py
@@ -0,0 +1,10 @@
+celcius = 50.00
+celcius = float(input ("masukan suhu dalam celcius"))
+fahrenheit = 0.0
+reamur = 0.0
+
+fahrenheit = 9/5 * celcius + 32
+reamur = 4/5 * celcius
+
+print ("suhu fahrenheit : %.2f"%fahrenheit)
+print ("Suhu Reamur: %.2f"%reamur)
